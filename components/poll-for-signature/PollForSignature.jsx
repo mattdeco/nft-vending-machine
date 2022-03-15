d629759efe25d4a0f6647a97b3c84cf6397b0b08
Matt Rosenzweig <m@tripleoptic.net>
2022-03-14 21:43:11 -0700

    Update QR screen with additional copy and content

--- components/poll-for-signature/PollForSignature.jsx
+++ components/poll-for-signature/PollForSignature.jsx
@@ -5,6 +5,7 @@ import ContentContainer from "../content-container/ContentContainer";
 import QRCode from "../qr-code/QRCode";
 import Instruction from "../instruction/Instruction";
 import styles from "./PollForSignature.module.scss";
+import SupportedWallets from "../supported-wallets/SupportedWallets";
 
 const PollForSignature = ({ qrCodeParams }) => {
   const [currentInstruction, setCurrentInstruction] = useState(1);
@@ -24,6 +25,19 @@ const PollForSignature = ({ qrCodeParams }) => {
 
   return (
     <div className={styles.container}>
+      <ContentContainer autoHeight={false}>
+        <div className={styles["content"]}>
+          <h1 className={styles["headline"]}>NFT Vending Machine</h1>
+          <h2 className={styles["subhead"]}>
+            All proceeds will be sent to the <strong>#HELPUKRAINE</strong> fund
+            <br />
+            supported by Solana and Metaplex.
+          </h2>
+          <h3 className={styles["body"]}>
+            Pay [x] SOL and you’ll receive an NFT from the available supply.
+          </h3>
+        </div>
+      </ContentContainer>
       <ContentContainer>
         <QRCode qrCodeParams={qrCodeParams} />
       </ContentContainer>
@@ -35,6 +49,10 @@ const PollForSignature = ({ qrCodeParams }) => {
             stepText={INSTRUCTION_STEPS[currentInstruction - 1]}
           />
         </AnimatePresence>
+      </div>
+      <SupportedWallets />
+      <div className={styles["credit"]}>
+        <p className={styles["credit-line"]}>built by @mattdeco</p>
       </div>
     </div>
   );
