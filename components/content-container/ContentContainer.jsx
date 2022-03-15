5882a5b66a4844c7b3f8862b1d033eb3018882bb
Matt Rosenzweig <m@tripleoptic.net>
2022-03-14 21:41:03 -0700

    Add additional layout support to ContentContainer

--- components/content-container/ContentContainer.jsx
+++ components/content-container/ContentContainer.jsx
@@ -1,20 +1,27 @@
 import propTypes from "prop-types";
 import styles from "./ContentContainer.module.scss";
 
-const ContentContainer = ({ children, mode }) => {
+const ContentContainer = ({ children, className, autoHeight, mode }) => {
   return (
-    <div className={`${styles["content-container"]} ${styles[mode]}`}>
+    <div
+      className={`${styles["content-container"]} ${
+        !autoHeight ? styles["auto-height"] : ""
+      } ${styles[mode]} ${className}`}
+    >
       {children}
     </div>
   );
 };
 
 ContentContainer.propTypes = {
+  autoHeight: propTypes.bool,
   mode: propTypes.oneOf(["success", "error", "default"]),
 };
 
 ContentContainer.defaultProps = {
+  className: "",
   mode: "default",
+  autoHeight: true,
 };
 
 export default ContentContainer;
